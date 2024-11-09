3bc6764061068470edb9b49c1dc0deb38e5976ba
username <178353419+while-maybe@users.noreply.github.com>
2024-11-09 12:44:19 +0000

    feat: add more methods to models

--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,10 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
