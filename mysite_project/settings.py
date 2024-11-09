34ee768ff6f9dc2ffa24357013d47b25abe5f4af
username <178353419+while-maybe@users.noreply.github.com>
2024-11-09 12:42:58 +0000

    feat: add models

--- mysite_project/settings.py
+++ mysite_project/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    # my apps
+    'blog',
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
