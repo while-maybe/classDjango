94218fb230ce7de65ef4d3495e21f1b81848bbf5
username <178353419+while-maybe@users.noreply.github.com>
2024-11-09 13:58:21 +0000

    feat: add urls, templates

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from blog.models import Post
+
+# add the PostAdmin class here
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_date')
+
 # Register your models here.
+admin.site.register(Post, PostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from blog.models import Post
+
 # Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    # return HttpResponse(f"List of posts: {posts}")
+    return render(request, 'blog/post_list.html', {'posts': posts})
